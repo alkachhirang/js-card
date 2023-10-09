0e2ba8751670fb343b7309b89b89ce2d00e088a5
alkachhirang <alkachhirang3@gmail.com>
2023-10-09 11:59:20 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+// for (let a = 2; a < 21; a++) {
+//     console.log('value of loop is $(a)');
+//     for (let i = 1; i <= 21; i++) {
+//         console.log(a + "*" + i + "*" + i * a);
+//     }
+// }
+
+var cards = [
+
+    {
+        Img: './assets/images/png/rose.png',
+        h2: 'Rose-garden',
+        p: 'Smells like heaven',
+    },
+    {
+        Img: './assets/images/png/rose.png',
+        h2: 'Rose-garden',
+        p: 'Smells like heaven',
+    },
+    {
+        Img: './assets/images/png/rose.png',
+        h2: 'Rose-garden',
+        p: 'Smells like heaven',
+    },
+
+]
+const mycontainer = document.querySelector('.cardcontainer');
+const mycard =
+    cards.map((postdata) => {
+
+        return `
+        <div class="cards">
+            <img class="w_100 max_width_250 border-radius_10" src=${postdata.Img} alt="" />
+            <h2 class="pt_20 d-flex align-items-center justify-content-center flex-column">${postdata.h2}</h2>
+            <p class="d-flex align-items-center justify-content-center flex-column">${postdata.p}</p>
+
+        </div>
+        `
+    }).join(" ")
+mycontainer.innerHTML = mycard;
+
